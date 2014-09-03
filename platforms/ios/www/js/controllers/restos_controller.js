489c96533ca8bb15ea4e544ae704f69076ecebdb
Sebaitu11 <sebaitu11@gmail.com>
2014-09-02 23:55:24 -0400

    changes

--- platforms/ios/www/js/controllers/restos_controller.js
+++ platforms/ios/www/js/controllers/restos_controller.js
@@ -1,25 +1,24 @@
 angular.module('restoApp.controllers')
 
-.controller('RestosCtrl', function($scope,Restos,$stateParams,$ionicNavBarDelegate,LoadingService) {
+.controller('RestosCtrl', function($scope,Restos,$stateParams,LoadingService) {
   
   $scope.$root.tabsHidden = "tabs-item-hide";
   $scope.showSearch = false;
-  LoadingService.show()
+  LoadingService.show();
+  $scope.dataIsThere = false;
   
   Restos.all($stateParams.barrioId).then(function(response){
     $scope.restos = response;  
+    
+    $scope.premRestos = response.filter(function(obj){
+      return obj.is_premium == true
+    })
+    //flag para mostrar slide-box cuando hay data
+    $scope.dataIsThere = true;
     LoadingService.hide()
   });
 
   $scope.switchSearch = function(search){
     $scope.showSearch = !search
-  }
-  $scope.getPreviousTitle = function() {
-    return $ionicNavBarDelegate.getPreviousTitle();
-  };
-  $scope.goBack = function(){
-    debugger
-    $ionicNavBarDelegate.back()
-  }
-
+  } 
 })
